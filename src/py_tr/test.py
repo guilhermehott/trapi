1af84e2c239e030ba661d59c3146614c4d21ea7a
Guilherme Hott <guilhermehott@gmail.com>
2020-11-07 15:54:43 +0100

    test and sample data

--- src/py_tr/test.py
+++ src/py_tr/test.py
@@ -6,16 +6,30 @@ from py_tr import TradeRepublicApi
 
 tr = TradeRepublicApi()
 
+
+def save_to_file(file, response):
+    with open(file, 'w') as outfile:
+        json.dump(response, outfile)
+
+
 async def my_loop():
     logging.info("async started")
     underlying_isin = "US0378331005"
-    await tr.search_derivative(underlying_isin)
-    # await tr.search("DE000TT0GR32", asset_type="derivative")
+    # await tr.search_derivative(underlying_isin)
+    # await tr.search("AAPL", asset_type="stock")
+    # await tr.ticker("DE000TT17QS7", exchange="LSX")
+    await tr.instrument_details("DE000TT17QS7")
+    # await tr.instrument_suitability("DE000TT0GQ58")
+    # await tr.derivative_details("DE000TT0GQ58")
+    # await tr.performance("DE000TT17QS7")
+    # await tr.portfolio()
 
     while True:
         subscription_id, subscription, response = await tr.recv()
         pprint.pprint(response)
-        with open('warrants_'+underlying_isin+'.txt', 'w') as outfile:
-            json.dump(response, outfile)
+        # save_to_file("portfolio.json", response)
+        # with open('warrants_'+underlying_isin+'.json', 'w') as outfile:
+        #     json.dump(response, outfile)
+
 
 asyncio.get_event_loop().run_until_complete(my_loop())
